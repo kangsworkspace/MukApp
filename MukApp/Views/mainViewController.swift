357b4388ea9c7cb67d08104909eb7b0b0bf2cdaa
kangsworkspace <141600830+kangsworkspace@users.noreply.github.com>
2023-10-23 22:39:23 +0900

    Add PickerView
    todo
    피커뷰 데이터 전달 처리
    coreData 데이터 처리

--- MukApp/Views/mainViewController.swift
+++ MukApp/Views/mainViewController.swift
@@ -9,8 +9,30 @@ import UIKit
 
 class mainViewController: UIViewController {
 
+    // ToDo
+    // 에러 처리 (빈 문자열 저장 안되도록)
+    // 카테고리 여러개 저장 - 출력
+    // 목록을 보여주는 뷰(피커 뷰)
+    
+    
     
     // MARK: - Interface
+    // 카테고리를 표시할 피커뷰
+    lazy var pickerView: UIPickerView = {
+        let picker = UIPickerView()
+        
+        picker.backgroundColor = .white
+        picker.delegate = self
+        picker.dataSource = self
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        return picker
+    }()
+    
+    // 테스트
+    private let testValues: [String] = ["A", "B", "C"]
+    
+    
+    
     // 메인 레이블 (mainLabel)
     private var mainLabel: UILabel = {
         let label = UILabel()
@@ -45,6 +67,24 @@ class mainViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    // categoryTextLabel
+    var categoryTextLabel: UILabel = {
+       let label = UILabel()
+        
+        // 텍스트 설정
+        label.text = "최소 1개의 카테고리를 정해주세요"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textAlignment = .center
+        
+        // 보더 설정
+        label.layer.borderWidth = 1
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     
     // 카테고리 뷰(categoryView)
     private var categoryView: UIView = {
@@ -82,19 +122,23 @@ class mainViewController: UIViewController {
         setAutoLayout()
     }
     
+    // 셋업 - 애드뷰
     func setAddView() {
         view.addSubview(mainLabel)
         view.addSubview(menuLabel)
+        view.addSubview(categoryTextLabel)
+        categoryView.addSubview(pickerView)
         view.addSubview(categoryView)
         view.addSubview(menuButton)
     }
     
+    // 셋업 - 오토 레이아웃
     func setAutoLayout() {
         
         // mainLabel 오토 레이아웃
         NSLayoutConstraint.activate([
             mainLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
-            mainLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            mainLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
             mainLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
             
             mainLabel.heightAnchor.constraint(equalToConstant: 50)
@@ -109,25 +153,59 @@ class mainViewController: UIViewController {
             menuLabel.heightAnchor.constraint(equalToConstant: 50)
         ])
         
+        // categoryTextLabel 오토 레이아웃
+        NSLayoutConstraint.activate([
+            categoryTextLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+            categoryTextLabel.topAnchor.constraint(equalTo: menuLabel.bottomAnchor, constant: 30),
+            categoryTextLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+            
+            categoryTextLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
         // categoryView 오토 레이아웃
         NSLayoutConstraint.activate([
             categoryView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
-            categoryView.topAnchor.constraint(equalTo: menuLabel.bottomAnchor, constant: 50),
+            categoryView.topAnchor.constraint(equalTo: categoryTextLabel.bottomAnchor, constant: 10),
             categoryView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
             
-            categoryView.heightAnchor.constraint(equalToConstant: 100)
+            categoryView.heightAnchor.constraint(equalToConstant: 80)
+        ])
+        
+        
+        
+        // pickerView 오토 레이아웃
+        NSLayoutConstraint.activate([
+            pickerView.leadingAnchor.constraint(equalTo: categoryView.leadingAnchor, constant: 0),
+            pickerView.topAnchor.constraint(equalTo: categoryView.topAnchor, constant: 0),
+            pickerView.bottomAnchor.constraint(equalTo: categoryView.bottomAnchor, constant: 0),
+            
+            pickerView.widthAnchor.constraint(equalToConstant: 100)
         ])
         
         // menuButton 오토 레이아웃
         NSLayoutConstraint.activate([
             menuButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
             menuButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
-            menuButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            menuButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
             
-            menuButton.heightAnchor.constraint(equalToConstant: 80)
+            menuButton.heightAnchor.constraint(equalToConstant: 60)
         ])
-        
-
     }
+}
+
+extension mainViewController: UIPickerViewDelegate {
+    
+}
+
+extension mainViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return testValues.count
+    }
+    
+    
 }
 
