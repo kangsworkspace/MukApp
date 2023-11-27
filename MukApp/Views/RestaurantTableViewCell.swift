adb8c27db37016393965de66d53c77e659bc2588
kangsworkspace <141600830+kangsworkspace@users.noreply.github.com>
2023-11-27 13:56:18 +0900

    Restaurant 화면 바꾸기 전 백업

---
+++ MukApp/Views/RestaurantTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantTableViewCell.swift
+//  MukApp
+//
+//  Created by Kang on 11/26/23.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
